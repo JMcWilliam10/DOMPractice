bbf904462375c64c72f16aab12449336e42ef6ab
Jonathan McWilliam <jonathanmcwilliam@gmail.com>
2019-03-27 07:29:10 -0400

    add js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const heading = document.querySelector('h1');
 
 heading.style.color = 'red';
+const color = 'blue';
+
+switch(color){
+    case 'red':
+    console.log('color is red');
+    break;
+    case 'blue':
+    console.log('color is blue');
+    break;
+    default:
+    console.log('color is not red or blue')
+}
